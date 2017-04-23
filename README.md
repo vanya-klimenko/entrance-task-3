0d95ab1cc981d03fd5d67a2aa71b751ef9416109
Vanya Klimenko <v@vanyaklimenko.ru>
2017-04-24 03:41:26 +0500

    Fix all found bugs
    Fixed caching, implemented favorites:remove event and much more.
    
    See the README for detailed changelog w/explanations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Задание 3
+# Задание 3  ·  ШРИ-2017
 
 Мобилизация.Гифки – сервис для поиска гифок в перерывах между занятиями.
 
@@ -22,23 +22,82 @@
 Это можно сделать с помощью встроенного в WebStorm/Idea веб-сервера, с помощью простого сервера
 из состава PHP или Python. Можно воспользоваться и любым другим способом.
 
-# 
+## Решение
+Перенёс воркера в корень, чтобы он увидел остальные файлы приложения. Потом нашёл, где он регистрируется и поправил путь → `blocks.js:469`
+  
+Добавил `gifs.html` в функцию `needStoreForOffline`, чтобы тот кешировался → `service-worker.js:146`
+  
+Нашёл ошибку, из-за которой воркер ничего не фетчил, даже если была сеть — достаточно было поменять местами функции в операторе *ИЛИ*:
 ```javascript
-		// Вопрос №1: зачем нужен этот вызов?
-			.then(() => self.skipWaiting())
-		// Вопрос №2: зачем нужен этот вызов?
-			self.clients.claim();
-		// Вопрос №3: для всех ли случаев подойдёт такое построение ключа?
-			const cacheKey = url.origin + url.pathname;
-		// Вопрос №4: зачем нужна эта цепочка вызовов?
-			return Promise.all(
-			    names.filter(name => name !== CACHE_VERSION)
-			        .map(name => {
-			            console.log('[ServiceWorker] Deleting obsolete cache:', name);
-			            return caches.delete(name);
-			        })
-			);
-		// Вопрос №5: для чего нужно клонирование?
-			cache.put(cacheKey, response.clone());
+// service-worker.js:41
+if (needStoreForOffline(cacheKey)) {
+    response = caches.match(cacheKey)
+        .then(cacheResponse => fetchAndPutToCache(cacheKey, event.request) || cacheResponse);
+…
+```
+
+Потом увидел событие `favorite:add`, но не увидел `favorite:remove`. Добавил, повесил обработчик — теперь гифки больше не оставаются в кеше после того, как их удаляли из избранного. 
+
+Ещё увеличил значение постоянной `CACHE_VERSION`, чтобы затриггерить функцию `deleteObsoleteCaches()`.
+
+---
+
+Выполнил дополнительное задание. Чтобы приложение могло переходить в офлайн-режим после первого запроса, просим его кешировать все файлы по самом первому событию, `install` → `service-worker.js:14`  
+И, собственно, кешируем, самостоятельно реализовав такой метод:
+```javascript
+const assets = [ '/gifs.html', '//yastatic.net/jquery/3.1.0/jquery.min.js',
+                 '/assets/blocks.js', '/assets/templates.js', '/assets/star.svg',
+                 '/assets/style.css' ]
+
+function preCacheAllAssets() {
+    return caches.open(CACHE_VERSION)
+        .then(cache => {
+            return cache.addAll(assets)
+    })
+}
+```
+  
+  
+## Ответы на вопросы
+```javascript
+// Вопрос №1: зачем нужен этот вызов?
+self.skipWaiting()
+```
+— Для активации нового воркера *сразу* после установки.
+<br><br>
+  
+```javascript
+// Вопрос №2: зачем нужен этот вызов?
+self.clients.claim()
+```
+— Для того, чтобы воркер смог управлять всеми страницами — которые он может увидеть — сразу, без перезагрузки.
+<br><br>
+
+```javascript
+// Вопрос №3: для всех ли случаев подойдёт такое построение ключа?
+const cacheKey = url.origin + url.pathname
+```
+— Да, только если в урле не окажется какого-нибудь параметра.
+<br><br>
+
+```javascript
+// Вопрос №4: зачем нужна эта цепочка вызовов?
+return Promise.all(
+    names.filter(name => name !== CACHE_VERSION)
+        .map(name => {
+            console.log('[ServiceWorker] Deleting obsolete cache:', name)
+            return caches.delete(name)
+        })
+);
+```
+— Чтобы очищать весь кэш каждый раз, когда меняется постоянная CACHE_VERSION.
+<br><br>
+
+```javascript
+// Вопрос №5: для чего нужно клонирование?
+cache.put(cacheKey, response.clone())
+```
+— Для того, чтобы не потерять значение ответа. Яваскрипт считает `response` потоком, [не буферизирует его содержимое](https://jakearchibald.com/2014/reading-responses/) и, как следствие, не даёт обращаться к нему больше одного раза.
+<br><br>
 		
 ```
--- service-worker.js
+++ service-worker.js
@@ -4,55 +4,60 @@
  * Сервис-воркер, обеспечивающий оффлайновую работу избранного
  */
 
-const CACHE_VERSION = '1.0.0-broken';
+const CACHE_VERSION = '1.0.1'
 
-importScripts('../vendor/kv-keeper.js-1.0.4/kv-keeper.js');
+importScripts('../vendor/kv-keeper.js-1.0.4/kv-keeper.js')
 
 
 self.addEventListener('install', event => {
-    const promise = preCacheAllFavorites()
+    const promise = preCacheAllAssets()
+        .then (() => preCacheAllFavorites())
         // Вопрос №1: зачем нужен этот вызов?
         .then(() => self.skipWaiting())
-        .then(() => console.log('[ServiceWorker] Installed!'));
+        .then(() => console.log('[ServiceWorker] Installed!'))
 
-    event.waitUntil(promise);
+     event.waitUntil(promise);
 });
 
 self.addEventListener('activate', event => {
     const promise = deleteObsoleteCaches()
         .then(() => {
             // Вопрос №2: зачем нужен этот вызов?
-            self.clients.claim();
+            self.clients.claim()
 
-            console.log('[ServiceWorker] Activated!');
-        });
+            console.log('[ServiceWorker] Activated!')
+        })
 
-    event.waitUntil(promise);
-});
+    event.waitUntil(promise)
+})
 
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
+    const url = new URL(event.request.url)
 
     // Вопрос №3: для всех ли случаев подойдёт такое построение ключа?
-    const cacheKey = url.origin + url.pathname;
+    const cacheKey = url.origin + url.pathname
 
-    let response;
+    let response
     if (needStoreForOffline(cacheKey)) {
         response = caches.match(cacheKey)
-            .then(cacheResponse => cacheResponse || fetchAndPutToCache(cacheKey, event.request));
+            .then(cacheResponse => fetchAndPutToCache(cacheKey, event.request) || cacheResponse);
     } else {
-        response = fetchWithFallbackToCache(event.request);
+        response = fetchWithFallbackToCache(event.request)
     }
 
-    event.respondWith(response);
-});
+    event.respondWith(response)
+})
 
 self.addEventListener('message', event => {
-    const promise = handleMessage(event.data);
+    const promise = handleMessage(event.data)
 
-    event.waitUntil(promise);
-});
+    event.waitUntil(promise)
+})
 
+/* Файлы проекта */
+const assets = [ '/gifs.html', '//yastatic.net/jquery/3.1.0/jquery.min.js',
+                 '/assets/blocks.js', '/assets/templates.js', '/assets/star.svg',
+                 '/assets/style.css' ]
 
 // Положить в новый кеш все добавленные в избранное картинки
 function preCacheAllFavorites() {
@@ -65,9 +70,16 @@ function preCacheAllFavorites() {
                 .then(cache => {
                     return Promise.all(
                         responses.map(response => cache.put(response.url, response))
-                    );
-                });
-        });
+                    )
+                })
+        })
+}
+
+function preCacheAllAssets() {
+    return caches.open(CACHE_VERSION)
+        .then(cache => {
+            return cache.addAll(assets)
+    })
 }
 
 // Извлечь из БД добавленные в избранное картинки
@@ -75,21 +87,21 @@ function getAllFavorites() {
     return new Promise((resolve, reject) => {
         KvKeeper.getKeys((err, keys) => {
             if (err) {
-                return reject(err);
+                return reject(err)
             }
 
             const ids = keys
                 .filter(key => key.startsWith('favorites:'))
                 // 'favorites:'.length == 10
-                .map(key => key.slice(10));
+                .map(key => key.slice(10))
 
             Promise.all(ids.map(getFavoriteById))
                 .then(urlGroups => {
-                    return urlGroups.reduce((res, urls) => res.concat(urls), []);
+                    return urlGroups.reduce((res, urls) => res.concat(urls), [])
                 })
-                .then(resolve, reject);
-        });
-    });
+                .then(resolve, reject)
+        })
+    })
 }
 
 // Извлечь из БД запись о картинке
@@ -97,15 +109,15 @@ function getFavoriteById(id) {
     return new Promise((resolve, reject) => {
         KvKeeper.getItem('favorites:' + id, (err, val) => {
             if (err) {
-                return reject(err);
+                return reject(err)
             }
 
-            const data = JSON.parse(val);
-            const images = [data.fallback].concat(data.sources.map(item => item.url));
+            const data = JSON.parse(val)
+            const images = [data.fallback].concat(data.sources.map(item => item.url))
 
-            resolve(images);
-        });
-    });
+            resolve(images)
+        })
+    })
 }
 
 // Удалить неактуальный кеш
@@ -116,18 +128,19 @@ function deleteObsoleteCaches() {
             return Promise.all(
                 names.filter(name => name !== CACHE_VERSION)
                     .map(name => {
-                        console.log('[ServiceWorker] Deleting obsolete cache:', name);
-                        return caches.delete(name);
+                        console.log('[ServiceWorker] Deleting obsolete cache:', name)
+                        return caches.delete(name)
                     })
-            );
-        });
+            )
+        })
 }
 
 // Нужно ли при скачивании сохранять ресурс для оффлайна?
 function needStoreForOffline(cacheKey) {
     return cacheKey.includes('vendor/') ||
         cacheKey.includes('assets/') ||
-        cacheKey.endsWith('jquery.min.js');
+        cacheKey.endsWith('jquery.min.js') ||
+        cacheKey.endsWith('gifs.html') /* Добавил главную страницу */
 }
 
 // Скачать и добавить в кеш
@@ -137,39 +150,40 @@ function fetchAndPutToCache(cacheKey, request) {
             return caches.open(CACHE_VERSION)
                 .then(cache => {
                     // Вопрос №5: для чего нужно клонирование?
-                    cache.put(cacheKey, response.clone());
+                    cache.put(cacheKey, response.clone())
                 })
-                .then(() => response);
+                .then(() => response)
         })
         .catch(err => {
-            console.error('[ServiceWorker] Fetch error:', err);
-            return caches.match(cacheKey);
-        });
+            console.error('[ServiceWorker] Fetch error:', err)
+            return caches.match(cacheKey)
+        })
 }
 
 // Попытаться скачать, при неудаче обратиться в кеш
 function fetchWithFallbackToCache(request) {
     return fetch(request)
         .catch(() => {
-            console.log('[ServiceWorker] Fallback to offline cache:', request.url);
-            return caches.match(request.url);
-        });
+            console.log('[ServiceWorker] Fallback to offline cache:', request.url)
+            return caches.match(request.url)
+        })
 }
 
 // Обработать сообщение от клиента
 const messageHandlers = {
     'favorite:add': handleFavoriteAdd
-};
+    'favorite:remove': handleFavoriteRemove
+}
 
 function handleMessage(eventData) {
-    const message = eventData.message;
-    const id = eventData.id;
-    const data = eventData.data;
+    const message = eventData.message
+    const id = eventData.id
+    const data = eventData.data
 
-    console.log('[ServiceWorker] Got message:', message, 'for id:', id);
+    console.log('[ServiceWorker] Got message:', message, 'for id:', id)
 
-    const handler = messageHandlers[message];
-    return Promise.resolve(handler && handler(id, data));
+    const handler = messageHandlers[message]
+    return Promise.resolve(handler && handler(id, data))
 }
 
 // Обработать сообщение о добавлении новой картинки в избранное
@@ -179,14 +193,28 @@ function handleFavoriteAdd(id, data) {
             const urls = [].concat(
                 data.fallback,
                 (data.sources || []).map(item => item.url)
-            );
+            )
 
             return Promise
                 .all(urls.map(url => fetch(url)))
                 .then(responses => {
                     return Promise.all(
                         responses.map(response => cache.put(response.url, response))
-                    );
-                });
+                    )
+                })
+        })
+}
+
+/* ...и об её удалении оттуда */
+function handleFavoriteRemove(id, data) {
+    return caches.open(CACHE_VERSION)
+        .then(cache => {
+            const urls = [].concat(
+                data.fallback,
+                (data.sources || []).map(item => item.url)
+            );
+
+            return Promise
+                .all(urls.map(url => cache.delete(url)))
         });
 }
